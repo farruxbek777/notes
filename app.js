6dc0e7586a27da47d78efdeb9966ed41c7b6b986
Farrukhbek Salimov <00010669@wiut.uz>
2021-03-05 17:51:06 +0500

    add styles

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.use('/static', express.static('public'))
+
+app.set('view engine', 'pug')
+
+
+app.get('/', (req, res) => {
+	res.render('home')
+	})
+
+app.listen(8000, err => {
+	if (err) throw err
+
+    console.log('App is running...')
+	})
